f80576f38859e6ef61d2df3660f1c891b7e71b5c
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-19 15:45:14 +0200

    Settings

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+import axios from "axios";
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer
+}
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+@import "~@fortawesome/fontawesome-free/scss/fontawesome.scss";
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="btn btn-primary">
+        ce sto
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
